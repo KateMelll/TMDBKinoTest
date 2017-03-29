8d3ca0d9038cb3ca133d9568f4057bfda034d150
Kate <mail@mail.com>
2017-03-29 19:53:55 +0200

    Add title switching

--- kino/Movies/MoviesViewController.swift
+++ kino/Movies/MoviesViewController.swift
@@ -12,9 +12,20 @@ class MoviesViewController: UIViewController {
 
     
     @IBOutlet weak var toolbarView: UIToolbar!
+    @IBOutlet weak var segmentedCotrlView: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.updateTitle()
+        self.segmentedCotrlView.addTarget(self, action: #selector(self.segmentChanged), for: .valueChanged)
+    }
+
+    func updateTitle() {
+        self.navigationItem.title = self.segmentedCotrlView.titleForSegment(at: self.segmentedCotrlView.selectedSegmentIndex)
+    }
+    
+    func segmentChanged(sender: UISegmentedControl) {
+        self.updateTitle()
     }
 
     // MARK: - Navigation
