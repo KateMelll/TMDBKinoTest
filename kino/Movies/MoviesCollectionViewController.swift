56fc50db23ae94e9154d0535438f37a25a3aa10a
Kate <mail@mail.com>
2017-03-29 20:16:33 +0200

    Connect view controllers

--- kino/Movies/MoviesCollectionViewController.swift
+++ kino/Movies/MoviesCollectionViewController.swift
@@ -14,6 +14,7 @@ class MoviesCollectionViewController: UICollectionViewController, UICollectionVi
 
     var movies = [MTMovie]()
     var refreshControl: UIRefreshControl!
+    weak var moviesViewController: MoviesViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- kino/Movies/MoviesViewController.swift
+++ kino/Movies/MoviesViewController.swift
@@ -13,6 +13,7 @@ class MoviesViewController: UIViewController {
     
     @IBOutlet weak var toolbarView: UIToolbar!
     @IBOutlet weak var segmentedCotrlView: UISegmentedControl!
+    var collectionViewController: MoviesCollectionViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +21,25 @@ class MoviesViewController: UIViewController {
         self.segmentedCotrlView.addTarget(self, action: #selector(self.segmentChanged), for: .valueChanged)
     }
 
-    func updateTitle() {
-        self.navigationItem.title = self.segmentedCotrlView.titleForSegment(at: self.segmentedCotrlView.selectedSegmentIndex)
-    }
-    
     func segmentChanged(sender: UISegmentedControl) {
         self.updateTitle()
     }
 
+    func updateTitle() {
+        self.navigationItem.title = self.segmentedCotrlView.titleForSegment(at: self.segmentedCotrlView.selectedSegmentIndex)
+    }
+    
+//    func setMode(_ index: Int) {
+//        
+//    }
+    
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        if segue.identifier == "MoviesCollectionViewController" {
+            let destController = segue.destination as! MoviesCollectionViewController
+            destController.moviesViewController = self
+            self.collectionViewController = destController
+        }
     }
 }
