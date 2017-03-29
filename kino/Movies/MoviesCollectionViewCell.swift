3467ee42fba775ab0fe6437a8762815c84a17101
Kate <mail@mail.com>
2017-03-29 15:16:09 +0200

    Add Movies screen

---
+++ kino/Movies/MoviesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesCollectionViewCell.swift
+//  kino
+//
+//  Created by Kate on 29/03/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    
+}
