61c3584a858e126de260b878303761e2332f85c0
Kate <mail@mail.com>
2017-04-01 11:32:06 +0200

    Start actors

---
+++ kino/Actors/ActorsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ActorsCollectionViewCell.swift
+//  kino
+//
+//  Created by K on 01/04/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ActorsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var actorImageView: UIImageView!
+    @IBOutlet weak var actorNameLabel: UILabel!
+    @IBOutlet weak var characterLabel: UILabel!
+    
+}
---
+++ kino/Crew/DirectorsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DirectorsCollectionViewCell.swift
+//  kino
+//
+//  Created by K on 01/04/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class DirectorsCollectionViewCell: UICollectionViewCell {
+    
+}
