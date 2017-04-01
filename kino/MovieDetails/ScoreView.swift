cb710be5a64863e359fc81b142e20d454834b40e
Kate <mail@mail.com>
2017-04-01 09:45:09 +0200

    Fix interface

--- kino/MovieDetails/ScoreView.swift
+++ kino/MovieDetails/ScoreView.swift
@@ -21,7 +21,7 @@ class ScoreView: UIView {
     
     override func draw(_ rect: CGRect) {
         let center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
-        let radius: CGFloat = min(bounds.width + 5, bounds.height + 5)
+        let radius: CGFloat = min(bounds.width + 15, bounds.height + 15)
         let arcWidth: CGFloat = 75
         let start: CGFloat = 0 + CGFloat(M_PI_2)
         let end: CGFloat = self.end * progress * (2 * CGFloat(M_PI)) + CGFloat(M_PI_2)
